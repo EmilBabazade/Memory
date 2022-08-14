fec12fa204cb56b709b0648f9132f84f292dbafa
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-08-14 11:24:50 +0400

    client: redux and create post form

--- server/dist/index.js
+++ server/dist/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/api/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.2rqpu.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch(error => console.log(error.message));
